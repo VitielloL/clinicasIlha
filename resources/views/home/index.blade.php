4899c65d576640bb4dc1dcbcb98175fe5d0e3129
Lucas Vitiello <lucasvitielloo@gmail.com>
2024-03-28 22:06:00 -0300

    fix: h2 index home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12 mt-4">
-            <h2 class="text-center mb-4">Agenda Semanal do Consultório</h2>
+            <h2 class="text-center mb-4">Agenda Diária do Consultório</h2>
         </div>
     </div>
     <div class="row">
