15f4341c4f9bb9b44e24786d6a96ecc5c231a830
Lucas Vitiello <lucasvitielloo@gmail.com>
2024-04-03 00:29:10 -0300

    add presenca na tela de index de consulta

--- resources/views/consulta/index.blade.php
+++ resources/views/consulta/index.blade.php
@@ -20,23 +20,44 @@
                             <th>Paciente</th>
                             <th>Dia da Semana</th>
                             <th>Data da Consulta</th>
+                            <th>Profissional</th>
+                            <th>Paciente</th>
+                            <th>Falta Justificada</th>
                             <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ( $dados as $dado )
+                        @foreach ($dados as $consulta)
                         @php
-                        $linkReadMore = url('/consulta/' . $dado->id);
-                        $linkEditItem = url ('/consulta/editar/' . $dado->id);
-                        $linkRemoveItem = url ('/consulta/remover/' . $dado->id);
+                        $linkReadMore = url('/consulta/' . $consulta->id);
+                        $linkEditItem = url ('/consulta/editar/' . $consulta->id);
+                        $linkRemoveItem = url ('/consulta/remover/' . $consulta->id);
                         @endphp
                         <tr>
-                            <td style="vertical-align:middle">{{$dado->sala}}</td>
-                            <td style="vertical-align:middle">{{$dado->horario}}</td>
-                            <td style="vertical-align:middle">{{$dado->profissional->nome}}</td>
-                            <td style="vertical-align:middle">{{$dado->cliente->nome}}</td>
-                            <td style="vertical-align:middle">{{$dado->dia_semana}}</td>
-                            <td style="vertical-align:middle">{{$dado->data_consulta}}</td>
+                            <td style="vertical-align:middle">{{$consulta->sala}}</td>
+                            <td style="vertical-align:middle">{{$consulta->horario}}</td>
+                            <td style="vertical-align:middle">{{$consulta->profissional->nome}}</td>
+                            <td style="vertical-align:middle">{{$consulta->cliente->nome}}</td>
+                            <td style="vertical-align:middle">{{$consulta->dia_semana}}</td>
+                            <td style="vertical-align:middle">{{$consulta->data_consulta}}</td>
+                            <td style="vertical-align:middle">
+                                <div class="form-check form-switch ml-3">
+                                    <input class="form-check-input" type="checkbox" id="prof_frequencia_{{$consulta->id}}" name="prof_frequencia[]" value="{{$consulta->id}}" {{$consulta->prof_frequencia ? 'checked' : ''}}>
+                                    <label class="form-check-label" for="prof_frequencia_{{$consulta->id}}">Presente</label>
+                                </div>
+                            </td>
+                            <td style="vertical-align:middle">
+                                <div class="form-check form-switch ml-3">
+                                    <input class="form-check-input" type="checkbox" id="paciente_frequencia_{{$consulta->id}}" name="paciente_frequencia[]" value="{{$consulta->id}}" {{$consulta->paciente_frequencia ? 'checked' : ''}}>
+                                    <label class="form-check-label" for="paciente_frequencia_{{$consulta->id}}">Presente</label>
+                                </div>
+                            </td>
+                            <td style="vertical-align:middle">
+                                <div class="form-check form-switch ml-3">
+                                    <input class="form-check-input" type="checkbox" id="falta_justificada_{{$consulta->id}}" name="falta_justificada[]" value="{{$consulta->id}}" {{$consulta->falta_justificada ? 'checked' : ''}}>
+                                    <label class="form-check-label" for="falta_justificada_{{$consulta->id}}">Justificada</label>
+                                </div>
+                            </td>
                             <td>
                                 <div class="d-flex">
                                     <a href={{$linkReadMore}} class="btn btn-info mr-2"><i class="fa fa-eye mr-1 mb-0" aria-hidden="true"></i> Ver Mais</a>
@@ -54,3 +75,5 @@
     </div>
 </div>
 @endsection
+
+@vite(['resources/js/registraFaltas.js'])
