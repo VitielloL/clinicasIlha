d73afda4b548a8621313b23fe1b1b65f1f315a53
Lucas Vitiello <lucasvitielloo@gmail.com>
2024-04-03 21:06:05 -0300

    add frequencia por ordem de criacao home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,36 +28,36 @@ class HomeController extends Controller
         return view('home.index', compact('dados'));
     }
 
-    public function buscar(Request $request)
+        public function buscar(Request $request)
     {
         // Definindo o fuso horário para São Paulo (Brasília)
         date_default_timezone_set('America/Sao_Paulo');
-    
+
         // Obtém a data atual no fuso horário do servidor
         $dataAtual = Carbon::now()->format('Y-m-d');
-    
+
         // Verificar se houve algum parâmetro de busca enviado
         if ($request->filled('nome_profissional') || $request->filled('nome_cliente') || $request->filled('sala') || $request->filled('frequencia')) {
             $nomeProfissional = $request->input('nome_profissional');
             $frequencia = $request->input('frequencia');
             $nomeCliente = $request->input('nome_cliente');
             $sala = $request->input('sala');
-    
+
             // Aplicar a lógica de filtro conforme necessário
-            $query = Consulta::query();
-    
+            $query = Consulta::whereDate('data_consulta', $dataAtual);
+
             if ($nomeProfissional) {
                 $query->whereHas('profissional', function ($q) use ($nomeProfissional) {
                     $q->where('nome', 'like', '%' . $nomeProfissional . '%');
                 });
             }
-    
+
             if ($nomeCliente) {
                 $query->whereHas('cliente', function ($q) use ($nomeCliente) {
                     $q->where('nome', 'like', '%' . $nomeCliente . '%');
                 });
             }
-    
+
             if ($frequencia) {
                 $query->where('frequencia', $frequencia);
             }
@@ -65,23 +65,23 @@ class HomeController extends Controller
             if ($sala) {
                 $query->where('sala', $sala);
             }
-    
-            $query->whereDate('data_consulta', $dataAtual);
-    
+
             $dados = $query->paginate(10); // Paginar os dados
-    
+
             // Formatando a data para exibição na view
             foreach ($dados as $consulta) {
                 $consulta->data_consulta = Carbon::parse($consulta->data_consulta)->format('d/m/Y');
             }
         } else {
-            // Se nenhum parâmetro de busca foi fornecido, recuperar todos os dados paginados
-            $dados = Consulta::whereDate('data_consulta', $dataAtual)->paginate(10); // Paginar os dados
+            // Se nenhum parâmetro de busca foi fornecido, recuperar todos os dados paginados do dia atual
+            $dados = Consulta::whereDate('data_consulta', $dataAtual)
+                            ->paginate(10); // Paginar os dados
         }
-    
+
         // Retornar a view com os dados
         return view('home.index')->with('dados', $dados);
     }
+
     
     public function atualizarFrequencia(Request $request, Consulta $consulta)
     {
