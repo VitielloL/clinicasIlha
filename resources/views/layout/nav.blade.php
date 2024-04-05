53406b28579668484593041c8e985c33509e9042
Lucas Vitiello <lucasvitielloo@gmail.com>
2024-04-05 00:02:44 -0300

    add jquery min inves do slim

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -28,7 +28,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />
 
         <!-- Scripts -->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.full.min.js"></script>
         
